deca26e7bce31e1c2253d836783608e27caf1e02
hatemahriz <h.ahriz@rgu.ac.uk>
2017-12-20 11:34:40 +0000

    DB connection

--- connection.php
+++ connection.php
@@ -24,6 +24,11 @@ define('DB_PASSWORD', $connectstr_dbpassword);
 
 $db = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
 
+if ($db)
+{
+        echo "connection successful. \n";
+}
+
 if (!$db)
 {
     echo "Error connecting to the database.\n";
